8184a3a189b3284dbf0a47127715ebe1aa088a05
BlakeFunTeis <tk0wo3@gmail.com>
2023-01-31 21:32:21 +0800

    add: get call user address

--- day7.sol
+++ day7.sol
@@ -4,15 +4,25 @@ pragma solidity ^0.8.7;
 contract Day7 {
     address owner;
 
+    modifier isOnwer() {
+        require(owner == msg.sender, "Error");
+        _;
+    }
+
     constructor() {
         owner = msg.sender;
+        require(owner == msg.sender, "Error");
     }
 
     function getOwner() public view returns(address) {
         return owner;
     }
 
-    function getTomorrowUnixTimestamp() public view returns(uint) {
+    function getTomorrowUnixTimestamp() public isOnwer view returns(uint) {
         return block.timestamp + 86400;
+    }
+    
+    function getAddress() public view returns(address) {
+        return msg.sender;
     }
 }
