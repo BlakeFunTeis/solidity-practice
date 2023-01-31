d03a686e9c638bd4f9f5480f1cf1330d2e79da44
BlakeFunTeis <tk0wo3@gmail.com>
2023-01-31 18:31:23 +0800

    add: day7 - get owner address

--- day7.sol
+++ day7.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.7;
 
 contract Day7 {
+    address owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getOwner() public view returns(address) {
+        return owner;
+    }
 }
