2192da2b4ea3edb3bd069e4504144fdcfc6d0388
BlakeFunTeis <tk0wo3@gmail.com>
2023-02-01 17:46:58 +0800

    add: mint practice

---
+++ mint-exceptions-practice.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Exceptions {
+
+    address owner;
+
+    mapping (address => uint) whiteList;
+
+    uint mintLimit = 24;
+
+    uint mintTotal = 0;
+
+    uint mintPrice = 0.2 ether;
+
+    constructor() {
+        owner = msg.sender;        
+    }
+
+    modifier isCheckOwner() {
+        require(owner == msg.sender, "is not owner");
+        _;
+    }
+
+    modifier isCheckMint() {
+        require(block.timestamp >= 1672502400, "Error1");
+        require(mintTotal <= mintLimit, "Error2");
+        _;
+    }
+
+    function mint(uint _total) public isCheckMint payable returns(bool) {
+        require(_total > 0, "mint total < 0");
+        require(msg.value >= (mintPrice * _total), "Not enough ether");
+        whiteList[msg.sender] += _total;
+        return true;
+    }
+
+    function getTotal() public view returns(uint) {
+        return whiteList[msg.sender];
+    }
+
+    function getEther() public isCheckOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+}
